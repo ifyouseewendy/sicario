7538ebff0d9706c75f0e94b68ae44273061f57a7
wendi <ifyouseewendy@gmail.com>
2016-01-05 20:49:17 +0800

    model_type: TDD on margin and total_price

--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -1,3 +1,15 @@
 class ModelType < ActiveRecord::Base
   belongs_to :model
+
+  def organization
+    model.organization
+  end
+
+  def margin
+    organization.margin
+  end
+
+  def total_price
+    base_price * margin
+  end
 end
--- test/models/model_type_test.rb
+++ test/models/model_type_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ModelTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_margin
+    VCR.use_cassette("test_model_type_margin") do
+      margin = organizations(:one).margin
+      assert_equal model_types(:one).base_price*margin, model_types(:one).total_price
+    end
+  end
 end
