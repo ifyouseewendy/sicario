5df9ab5bf2bc6a486d3ed48732387e9aeec48cf5
wendi <ifyouseewendy@gmail.com>
2016-01-05 19:17:36 +0800

    organization: Add STI support

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  end
+
+  def test_on_sti
+    assert_raises ActiveRecord::SubclassNotFound, 'Invalid STI types' do
+      Organization.create!(type: "Hello")
+    end
+  end
 end
