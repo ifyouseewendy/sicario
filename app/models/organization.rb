1aae44ad379c6338d75f9d5ce68f128f10a22803
wendi <ifyouseewendy@gmail.com>
2016-01-05 19:30:01 +0800

    organization: Validates on pricing_policy

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,3 @@
 class Organization < ActiveRecord::Base
+  validates_inclusion_of :pricing_policy, in: %w(Flexible Fixed Prestige)
 end
--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -6,7 +6,16 @@ class OrganizationTest < ActiveSupport::TestCase
 
   def test_on_sti
     assert_raises ActiveRecord::SubclassNotFound, 'Invalid STI types' do
-      Organization.create!(type: "Hello")
+      Organization.create!(type: "Hello", pricing_policy: 'Fixed')
+    end
+  end
+
+  def test_validation_on_pricing_logic
+    assert_silent do
+      Organization.create!(type: "Service", pricing_policy: 'Fixed')
+    end
+    assert_raises ActiveRecord::RecordInvalid, 'Invalid pricing_policy' do
+      Organization.create!(type: "Service", pricing_policy: 'Hello')
     end
   end
 end
