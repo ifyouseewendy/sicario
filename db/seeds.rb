5aacacf7f5b956969397486e0af9325991d6ec12
wendi <ifyouseewendy@gmail.com>
2016-01-07 17:36:37 +0800

    seed: Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Organization.destroy_all
+
+%w(Service ShowRoom Service) \
+  .zip( %w(Flexible Fixed Prestige) ) \
+  .each_with_index \
+  .reduce([]) do |ar, ((type, pricing_policy), idx)|
+    name = "Org #{idx}"
+
+    org = Organization.create!(
+      name: name,
+      public_name: name,
+      type: type,
+      pricing_policy: pricing_policy
+    )
+
+    name = "serie_#{idx+1}"
+
+    model = org.models.create!(
+      name: name,
+      model_slug: name,
+    )
+
+    (1..2).each do |id|
+      name = "bmw_#{idx*2 + id}"
+
+      model.model_types.create!(
+        name: name,
+        model_type_slug: name,
+        model_type_code: nil,
+        base_price: 10000 * (id),
+      )
+    end
+  end
