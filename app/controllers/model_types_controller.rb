d73c40c9fb9de0835740ac52df8534637fa791c6
wendi <ifyouseewendy@gmail.com>
2016-01-07 15:20:49 +0800

    model_types: Fill data for index

--- app/controllers/model_types_controller.rb
+++ app/controllers/model_types_controller.rb
@@ -1,5 +1,30 @@
 class ModelTypesController < ApplicationController
   def index
-    render json: 'hello world'
+    model_types = model.model_types.reduce([]) do |ary, model_type|
+      ary << {
+        name: model_type.name,
+        total_price: model_type.total_price
+      }
+    end
+
+    render json: \
+      {
+        models: [
+          {
+            name: model.name,
+            model_types: model_types
+          }
+        ]
+      }
   end
+
+  private
+
+    def model
+      Model.friendly.find(permitted_params[:model_id])
+    end
+
+    def permitted_params
+      params.permit(:model_id)
+    end
 end
--- test/controllers/model_types_controller_test.rb
+++ test/controllers/model_types_controller_test.rb
@@ -1,10 +1,21 @@
 require 'test_helper'
 
 class ModelTypesControllerTest < ActionController::TestCase
+  def setup
+    models(:one).save # enable slug after init from fixtures
+
+    ModelType.any_instance.stubs(:total_price).returns(1000)
+  end
+
   def test_index
     assert_routing '/models/one/model_types', controller: 'model_types', action: 'index', model_id: 'one'
 
     get(:index, {model_id: 'one'})
     assert_response(:success)
+
+    data = JSON.parse(response.body)
+    assert_equal 1              , data['models'].count
+    assert_equal ModelType.count, data['models'][0]['model_types'].count
+    assert_equal 1000           , data['models'][0]['model_types'][0]['total_price']
   end
 end
